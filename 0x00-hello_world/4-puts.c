6c6435c7c74faa3e74748f6af36a8e3738d95545
Iam-Ebuka <ebukagrizz@gmail.com>
2022-09-22 12:49:12 -0700

    correction on using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 /**
  * main - prints the phrase "Programming is like building a multiligual puzzle
+ * @myStr: This is a character variable that holds
+ * the string that is to be printed out
+ *
  * Return: 0 if exited properly, non-zero otherwise"
  */
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
+	char myStr[51] = "Programming is like building a multilingual puzzle";
+
+	puts(myStr);
 	return (0);
 }
