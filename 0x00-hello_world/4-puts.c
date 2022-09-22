f54dfb47ca9d2123a3a1ca130b6b91c6c00f78fe
Iam-Ebuka <ebukagrizz@gmail.com>
2022-09-22 12:57:21 -0700

    correction to using puts in c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
  * main - prints the phrase "Programming is like building a multiligual puzzle
- * @myStr: This is a character variable that holds
- * the string that is to be printed out
+ * @myStr: string to be printed out
  *
  * Return: 0 if exited properly, non-zero otherwise"
  */
 int main(void)
 {
-	char myStr[51] = "Programming is like building a multilingual puzzle";
+	char myStr[51] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
 	return (0);
